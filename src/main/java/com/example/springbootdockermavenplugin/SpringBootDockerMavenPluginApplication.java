d96a3e72b266ceb7a596d400eb21300c9891073c
Rohan Kumar <rohaan@redhat.com>
2021-10-18 22:33:10 +0530

    Initial Commit
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ src/main/java/com/example/springbootdockermavenplugin/SpringBootDockerMavenPluginApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockermavenplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerMavenPluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerMavenPluginApplication.class, args);
+	}
+
+}
